0ae5aacc8c6fb9c58b31576c1ba9351ae1e6734d
Fábio Ciambeli <fciambeli@gmail.com>
2025-06-30 23:34:21 -0300

    feat: config route create user and controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -2,10 +2,19 @@ const create = (req, res) => {
     const {name, username, email, password, avatar, background} = req.body;
 
    if (!name || !username || !email || !password || !avatar || !background) {
-    res.json("Submmit all field for registration")
+    res.status(401).send({message: "Submmit all field for registration"})
    }
 
-    res.json("OK")
+    res.status(201).send({
+        message: "user created successfully",
+        user: {
+            name,
+            username,
+            email,
+            avatar,
+            background,
+        },
+    })
 };
 
 module.exports = { create };
