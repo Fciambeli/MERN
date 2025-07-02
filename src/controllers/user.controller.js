f0b8e33dc095b4912c2b99e315f61fb20b5f1d25
Fábio Ciambeli <fciambeli@gmail.com>
2025-07-01 23:28:17 -0300

    feat: added 2 get routes

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,13 +1,24 @@
-const create = (req, res) => {
-    const {name, username, email, password, avatar, background} = req.body;
+const userService = require('../services/user.service')
+const mongoose = require("mongoose");
 
-   if (!name || !username || !email || !password || !avatar || !background) {
-    res.status(401).send({message: "Submmit all field for registration"})
-   }
+const create = async (req, res) => {
+    const { name, username, email, password, avatar, background } = req.body;
+
+    if (!name || !username || !email || !password || !avatar || !background) {
+        res.status(401).send({ message: "Submmit all field for registration" })
+    }
+
+    const user = await userService.createService(req.body);
+
+    if (!user) {
+        return res.status(400).send({ message: "Error creating User" });
+    }
+
 
     res.status(201).send({
         message: "user created successfully",
         user: {
+            id: user._id,
             name,
             username,
             email,
@@ -17,6 +28,32 @@ const create = (req, res) => {
     })
 };
 
-module.exports = { create };
+const findAll = async (req, res) => {
+    const users = await userService.findAllService();
+
+    if (users.length === 0) {
+        return res.send.status(400).send({ message: "There are no registered users" });
+    }
+
+    res.send(users)
+};
+
+const findById = async (req, res) => {
+    const id = req.params.id;
+
+if(!mongoose.Types.ObjectId.isValid(id)){
+    return res.status(400).send({ message: "Invalid ID" });
+}
+
+    const user = await userService.findByIdService(id);
+
+    if (!user) {
+        return res.status(400).send({ message: "User not found" });
+    }
+
+    res.send(user);
+};
+
+module.exports = { create, findAll, findById };
 
 
---
+++ src/database/db.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const connectDatabase = () => {
+    console.log("Wait connecting to the database")
+
+    mongoose.connect(
+        "mongodb+srv://fciambeli:EeYlbaM3uhCRH1ut@cluster0.cajndrm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+        { useNewUrlParser: true, useUnifiedTopology: true }
+    )
+    .then(() => console.log("MongoDB Atlas Conneected"))
+    .catch((error) => console.log(error));
+};
+
+module.exports = connectDatabase;
