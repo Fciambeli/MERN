83fa0ea3483d7e323212bd7b8745c69596641a56
Fábio Ciambeli <fciambeli@gmail.com>
2025-08-25 18:09:24 -0300

    refactor: added the global variable

--- src/database/db.js
+++ src/database/db.js
@@ -2,10 +2,13 @@ import mongoose from "mongoose";
 
 const connectDatabase = () => {
     console.log("Wait connecting to the database")
-
-    mongoose.connect(
-        "mongodb+srv://fciambeli:EeYlbaM3uhCRH1ut@cluster0.cajndrm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
-        { useNewUrlParser: true, useUnifiedTopology: true }
+    console.log(process.env)
+    mongoose
+    .connect( process.env.MONGODB_URI ,
+        
+        { 
+            useNewUrlParser: true, useUnifiedTopology: true 
+        }
     )
     .then(() => console.log("MongoDB Atlas Conneected"))
     .catch((error) => console.log(error));
