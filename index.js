32ab0463b266cf9396f844860abf563f9b804452
Fábio Ciambeli <fciambeli@gmail.com>
2025-08-26 22:18:17 -0300

    feat: how to login and compare password with bcrypt

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 import express from "express";
 import connectDatabase from "./src/database/db.js";
-import userRoute from "./src/routes/user.route.js";
 import dotenv from "dotenv";
+
+import userRoute from "./src/routes/user.route.js";
+import authRoute from "./src/routes/auth.route.js";
+
+
 dotenv.config();
 
 const port = process.env.PORT || 3000;
@@ -11,6 +15,7 @@ connectDatabase();
 
 app.use(express.json());
 app.use("/user", userRoute);
+app.use("/auth", authRoute);
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
 
