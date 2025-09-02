455e7165f61f69f3e93db0bf39142172d98fc9a7
Fábio Ciambeli <fciambeli@gmail.com>
2025-09-02 19:16:09 -0300

    feat: added create News

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import dotenv from "dotenv";
 
 import userRoute from "./src/routes/user.route.js";
 import authRoute from "./src/routes/auth.route.js";
-
+import newsRoute from "./src/routes/news.route.js";
 
 dotenv.config();
 
@@ -16,6 +16,7 @@ connectDatabase();
 app.use(express.json());
 app.use("/user", userRoute);
 app.use("/auth", authRoute);
+app.use("/news", newsRoute);
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
 
